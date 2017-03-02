683689dab1c1eb0b8d52891af370e20d9c971c95
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-03-02 20:38:10 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+const session    = require("express-session");
+const passport   = require("passport");
+
+const authController = require("./routes/auth-controller");
+var index            = require('./routes/index');
+var users            = require('./routes/users');
+
+
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/passport-local");
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+require('./config/passport')(passport);
+
+app.use(session({
+  secret: "passport-local-strategy",
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', authController);
+app.use('/users', users);
+
+app.all('/*', function (req, res) { 
+  res.sendfile(__dirname + '/public/index.html');  
+}); 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
